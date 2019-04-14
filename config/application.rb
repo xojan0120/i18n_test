1a7c6518cdb73fbb3599cba9ee3c01a4e1739249
xojan0120 <xojan0120@gmail.com>
2019-04-14 15:40:37 +0900

    [Add] test code

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module I18nTest
     config.generators do |g| g.test_framework :rspec, view_specs: false, helper_specs: false, routing_specs: false, controller_specs: false end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
